d954d1623e90277974b98a9924b88b319d234598
Nathan Ferdinando <nferdica@outlook.com>
2024-08-20 15:36:25 -0300

    feat: Added movie list

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import api from '../../services/api'
+import { Link } from "react-router-dom";
+import './home.css'
 
 // movie/now_playing?api_key=ca38a0df1e260ab351f0bb8e4802412c
 
@@ -17,7 +19,8 @@ function Home() {
                 }
             })
 
-            console.log(response)
+            setFilmes(response.data.results.slice(0, 10))
+
         }
 
         loadFilmes();
@@ -25,8 +28,18 @@ function Home() {
     }, [])
 
     return(
-        <div>
-            <h1>Bem vindo!</h1>
+        <div className="container">
+            <div className="lista-filmes">
+                {filmes.map((filme) => {
+                    return (
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    )
+                })}
+            </div>
         </div>
     )
 }
